7cc9653c4e587dc24e861482dc519d7ff4c83225
MichaelPile <mjpile@gmail.com>
2023-12-20 00:45:42 -0800

    completes Friday 13th

--- src/index.js
+++ src/index.js
@@ -1,39 +1,79 @@
 // write your code here
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('http://localhost:3000/ramens')
+
+fetch('http://localhost:3000/ramens')
     .then(function(response){
         return response.json();
     })
-    .then(function(data){
-        for(const key in data){ //iterates over each object in data
-        //make a list with all the data from the object with key:value pairs where key is id and value is innerText?
-        let img = document.createElement('img'); 
-        img.src = key['image'];
-        img.id = key['id'];
-        document.querySelector('div#ramen-menu').appendChild(img); //should add the img to ramen-menu
+    .then((data) => renderRamens(data))
+
+function renderRamens(ramenArr) {
+
+    const ramenMenu = document.querySelector('#ramen-menu');
+
+    ramenArr.forEach((ramenObj) => {
+        // console.log(ramenObj);
+        // console.log(ramenObj.image);
+
+        const img = document.createElement('img');
+        img.src = ramenObj.image;
+
+        ramenMenu.appendChild(img);
+
+        img.addEventListener('click', handleClick);
+
+        function handleClick() {
+            const nameDisplayLocation = document.querySelector('.name');
+            const imgDisplayLocation = document.querySelector('.detail-image');
+            const restaurantDisplayLocation = document.querySelector('.restaurant');
+            const ratingDisplayLocation = document.querySelector('#rating-display');
+            const commentDisplayLocation = document.querySelector('#comment-display');
+
+            //console.log(nameDisplayLocation)
+
+            nameDisplayLocation.innerText = ramenObj.name;
+            imgDisplayLocation.src = ramenObj.image;
+            restaurantDisplayLocation.innerText = ramenObj.restaurant;
+            ratingDisplayLocation.innerText = ramenObj.rating;
+            commentDisplayLocation.innerText = ramenObj.comment;
         }
-    })
+    });
+}
 
-    document.querySelector('div#ramen-menu').addEventListener('click', displayRamenInfo); //adds event listener for clicking on ramen-menu elements
+const form = document.querySelector('#new-ramen');
+form.addEventListener('submit', (e)=> addRamen(e))
 
-    function displayRamenInfo(e) { //changes display elements to equal the clicked menu element
-        let id = e.target.id;
-        
-        document.querySelector('img.detail-image').src = e.target.src;
-        document.querySelector('h2.name').innerText = 
-        document.querySelector('h3.restaurant').innerText =
-        document.querySelector('#rating-display').innerText = 
-        document.querySelector('#comment-display').innerText =
-        
+function addRamen(e) { //should add a new element to ramen menu based on the forms fields
+    e.preventDefault();
+
+    const newRamenObj = {
+        name : e.target.name.value,
+        restaurant :e.target.restaurant.value,
+        image : e.target.image.value,
+        rating : e.target.rating.value,
+        comment : e.target['new-comment'].value
     }
 
-    document.querySelector('#new-ramen').addEventListener('submit', addRamen); //listens for user submitting form
+    renderRamens([newRamenObj]);
+}
 
-    function addRamen(e) { //should add a new element to ramen menu based on the forms fields
-        e.preventDefault();
-        //same as .then(function(data) using the text in the input
-    }
-})
+
+
+
+
+
+
+
+
+//     document.querySelector('#new-ramen').addEventListener('submit', addRamen); //listens for user submitting form
+
+//     function addRamen(e) { //should add a new element to ramen menu based on the forms fields
+//         e.preventDefault();
+//         //same as .then(function(data) using the text in the input
+//         e.target.children[2].value; //Name input
+//         document.querySelector('#new-name').value; //Name input
+//         e.target.children[4].value; //restaurant input
+//     }
+
 
 
